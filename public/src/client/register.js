e5ace150bbd83cd8c0b51fbbc95fe0561c15a4fc
aadalbek <aadalbek@andrew.cmu.edu>
2025-09-22 22:33:08 +0300

    suggest username if taken

--- public/src/client/register.js
+++ public/src/client/register.js
@@ -135,7 +135,8 @@ define('forum/register', [
 				if (results.every(obj => obj.status === 'rejected')) {
 					showSuccess(usernameInput, username_notify, successIcon);
 				} else {
-					showError(usernameInput, username_notify, '[[error:username-taken]]');
+					const suggestion = username + '123';
+					showError(usernameInput, username_notify, '[[error:username-taken]] Try "${suggestion}" ');
 				}
 
 				callback();
